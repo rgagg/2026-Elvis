c608d72483359b9fd10e0a0f815cf3d7e0b1a86d
Richard Gagg <r.gagg@outlook.com>
2026-01-02 15:09:10 +1100

    Prof. John Gallauger's SwiftUI course. Ch.1 Peace , Love and Understanding challenge. Final.

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var message: String = ""
+  @State private var image: String = ""
+  
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Text("What's So Funny 'bout")
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .foregroundStyle(.purple)
+          
+          Spacer()
+          
+          Image(systemName: image)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 300, height: 300)
+          
+          Text(message)
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .foregroundStyle(.purple)
+          
+          Spacer()
+          
+          HStack {
+            Button("Peace") {
+              message = "Peace"
+              image = "peacesign"
+            }
+            
+            Button("Love") {
+              message = "Love"
+              image = "heart"
+            }
+            
+            Button("Understanding") {
+              message = "Understanding"
+              image = "lightbulb"
+            }
+            
+          }
+          .buttonStyle(.glassProminent)
+          .tint(.purple)
+          
         }
         .padding()
     }
