63ff95d65aa36fdf5275579a3d2918c809dba081
Richard Gagg <r.gagg@outlook.com>
2026-01-02 14:48:42 +1100

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Richard Gagg on 2/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
